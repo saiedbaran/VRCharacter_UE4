479d42b5ea6fcd39ee3425acfeeaa5ea24ce152e
Saeed Safikhani <s.safikhani@outlook.com>
2021-01-06 11:58:48 +0100

    VR Plugin initial Setup

---
+++ Plugins/VRCharacterPlugin/Source/VRCharacterPlugin/Private/VRHandAnimationInstance.cpp
@@ -0,0 +1,21 @@
+﻿#include "VRCharacterPlugin/Public/VRHandAnimationInstance.h"
+
+void FVRHandAnimationInstanceProxy::PreUpdate(UAnimInstance* InAnimInstance, const float DeltaSeconds)
+{
+	Super::PreUpdate(InAnimInstance, DeltaSeconds);
+
+	UVRHandAnimationInstance* AnimInstance = Cast<UVRHandAnimationInstance>(InAnimInstance);
+	if (IsValid(AnimInstance))
+	{
+		if (IsValid(AnimInstance->HandMotionController))
+		{
+			Grip = AnimInstance->HandMotionController->GetGripStat();
+			TypeOfGrab = AnimInstance->HandMotionController->GetTypeOfGrab();
+		}
+	}
+}
+
+void UVRHandAnimationInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+}
